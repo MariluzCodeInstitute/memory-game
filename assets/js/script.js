b056798deffda02d77a154181ad150139273ed16
Mariluz Herrera <gcsf2mk0@students.codeinstitute.net>
2024-09-14 18:06:19 +0000

    Add draft function for checking answer and eventlistener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 document.getElementById('btn-start-game').addEventListener('click', generateRandomNumber);
+document.getElementById('btn-submit').addEventListener('click', checkAnswer);
+
+
 
 function generateRandomNumber() {
     let num = Math.floor(Math.random() * 10);
@@ -10,4 +13,10 @@ function displayNumber(number) {
     setTimeout(() => {
         document.getElementById('number').innerText = '';
       }, 1000);
+}
+
+function checkAnswer(number) {
+    let userAnswer = parseInt(document.getElementById("player-answer").value);
+    let correct = userAnswer === number
+    console.log(correct)
 }
